24961405ecea904022d821a1e9f47daaf643d178
Dineshsethu001 <dineshsethu15981@gmail.com>
2024-11-21 18:40:17 +0530

    funtionality pending

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './Contact.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+import call_icon from '../../assets/call_icon.svg'
+
+const Contact = () => {
+  return (
+    <div id="contact" className="contact">
+      <div className="contact-title">
+        <h1>Get in Touch</h1>
+        <img src={theme_pattern} alt=""/>
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's talk</h1>
+          <p>I'm currently available to tale on new projects, So feel free to contact us</p>
+          <div className="contact-details">
+            <div className="contact-detail">
+                <img src={mail_icon} alt=""/><p>dineshsethu15981@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+            <img src={call_icon} alt=""/><p>+1111111110</p>
+            </div>
+            <div className="contact-detail">
+            <img src={location_icon} alt=""/><p>Tamil Nadu, India</p>
+            </div>
+          </div>
+        </div>
+        <form action="https://formspree.io/f/xrbglrzv"
+  method="POST" className="contact-right">
+
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your name " name ="name" />
+          <label htmlFor=''>Your Email</label>
+          <input type="email" placeholder='Enter your email' name="email"/>
+          <label htmlFor="">Write your message here</label>
+          <textarea name="message" placeholder='Enter your message' rows="8"/>
+          <button type="submit" className='contact-submit'>Submit now</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
         <img src={profile_img} alt="profile_img"/>
         <h1><span>I'm Dinesh Thanigaivel,</span> Frontend developer based in India</h1>
         <p>I am a frontend developer from Tamil Nadu, Vellore</p>
         <div className="hero-action">
-            <div className="hero-connect">Connect with me</div>
+            <div className="hero-connect"><AnchorLink className='anchorlink' offset={50} href="#contact">Connect with me</AnchorLink></div>
             <div className="hero-resume">My Resume</div>
         </div>
     </div>
